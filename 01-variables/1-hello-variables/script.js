047b904430db5e78d557d3c713ae57942aa5082d
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-14 11:23:17 +0200

    exercice variable

--- 01-variables/1-hello-variables/script.js
+++ 01-variables/1-hello-variables/script.js
@@ -1,6 +1,6 @@
 /*  Votre code Javascript vient ici:
      . - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+      - N'oubliez pas de sauvegarder pour afficher les changements...
 */
 let Name;
 let admin;
--- 01-variables/2-declare-variables/script.js
+++ 01-variables/2-declare-variables/script.js
@@ -1,6 +1,6 @@
 /*  .Votre code Javascript vient ici:
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+      - N'oubliez pas de sauvegarder pour afficher les changements...
 */
 let planète = "Terre";
 let User ="admin"; /*Username*/
