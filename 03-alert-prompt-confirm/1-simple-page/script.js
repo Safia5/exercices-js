10379b68cdc42890b4eab2112910b2de193220cb
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-14 12:14:31 +0200

    prompt alert fait

--- 03-alert-prompt-confirm/1-simple-page/script.js
+++ 03-alert-prompt-confirm/1-simple-page/script.js
@@ -2,3 +2,7 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let nom = prompt('quel es ton nom?');
+alert(`tu es ${nom} !`);
+/* ou let nom = prompt('Quel es ton nom ?');
+alert(nom);*/
