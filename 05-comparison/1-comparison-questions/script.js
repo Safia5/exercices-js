b7c52136d272a1cbcd43efbfca458066f0e8c818
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-14 15:11:50 +0200

    exercice comparaison fait

--- 05-comparison/1-comparison-questions/script.js
+++ 05-comparison/1-comparison-questions/script.js
@@ -2,3 +2,10 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+5 > 4 // true car 5 es supérieur à 4 //
+"apple" > "pineapple"//false car a et plus petit que p, a = 1//
+"2" > "12" // false 2 es plus petit que 12// coorection  /* true car 2 et pls grd que 1*/
+undefined == null // true car ils ont egaux que l'un l'autre //
+undefined === null// false car ils ont des types different undefined es NaN et null es 0//
+null == "\n0\n"// false car null es qu'egaux à undefined //
+null === +"\n0\n"//..// /*false il n'ont pas le mm type*/
