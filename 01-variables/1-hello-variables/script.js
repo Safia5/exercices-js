1e8f8a925b0ce4799938912151f6e87b3056b613
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-14 11:12:28 +0200

    exercice variable fait

--- 01-variables/1-hello-variables/script.js
+++ 01-variables/1-hello-variables/script.js
@@ -1,5 +1,5 @@
 /*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
+     . - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
 let Name;
--- 01-variables/2-declare-variables/script.js
+++ 01-variables/2-declare-variables/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
+/*  .Votre code Javascript vient ici:
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
