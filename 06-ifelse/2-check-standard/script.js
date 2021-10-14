cdb037727cea6c9be3d9039fd83c43ed3c34e356
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-14 16:51:57 +0200

    ex2 ifelse fait

--- 06-ifelse/2-check-standard/script.js
+++ 06-ifelse/2-check-standard/script.js
@@ -2,3 +2,10 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let message = prompt ('Quel est le nom "officiel" de JavaScript?','');
+if (message == `ECMAScript`) {
+      alert ( `Bonne réponse !` );
+}
+else{ 
+       alert (`Ne sait pas ? ECMAScript!`);
+} 
