6ef9367f5c0e622a1c5e6e2c506971a58dff2379
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-15 15:27:50 +0200

    exercice typeof

--- 08-type-conversions/script.js
+++ 08-type-conversions/script.js
@@ -2,3 +2,11 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let value = "123";
+alert(typeof str);
+
+let str = "123";
+alert(typeof Number (str));
+
+let nb = 1;
+alert(typeof Boolean (nb));
