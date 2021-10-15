b807f227e80ac07c9aa95f9a82275d5c3d1a6c91
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-15 10:46:10 +0200

    exer fait

--- 06-ifelse/3-sign/script.js
+++ 06-ifelse/3-sign/script.js
@@ -2,3 +2,5 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let resultat = prompt ('type number',"0"); // declare resultt type nombre
+// a refaire
