43a1d175dc7b8ac39e9c5555d74443541a919324
Safia <90760941+Safia5@users.noreply.github.com>
2021-10-17 18:49:25 +0200

    exercice switch fait

--- 09-switch/1-rewrite-switch-if-else/script.js
+++ 09-switch/1-rewrite-switch-if-else/script.js
@@ -2,3 +2,29 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+switch (browser) {
+  if ( browser === Edge ):
+    alert( "You've got the Edge!" );
+    break; {
+else if ( browser == 'Chrome'
+else if =='Firefox';
+else if =='Safari';
+else if == 'Opera'; 
+}
+  alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+} 
+                            // correction //
+/* if(browser == 'Edge') {
+  alert("You've got the Edge!");
+} else if (browser == 'Chrome'
+ || browser == 'Firefox'
+ || browser == 'Safari'
+ || browser == 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+} */
